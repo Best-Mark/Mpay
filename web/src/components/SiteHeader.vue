66d06b392f759ac3be70311714d618d6d628e60f
Best-Mark <15054300@qq.com>
2026-09-20 02:25:17 +0800

    feat(web): 官网头部补登录入口，移动端菜单内提供登录/入驻/文档入口

--- web/src/components/SiteHeader.vue
+++ web/src/components/SiteHeader.vue
@@ -12,11 +12,18 @@
         <router-link to="/scenarios" @click="close">应用场景</router-link>
         <router-link to="/docs" @click="close">接入文档</router-link>
         <router-link to="/about" @click="close">关于我们</router-link>
+        <!-- 小屏按钮被隐藏，入口放菜单里 -->
+        <a class="only-mobile" :href="site.consoleUrl" @click="close">登录</a>
+        <a class="only-mobile" :href="site.registerUrl" @click="close">商户入驻</a>
+        <a class="only-mobile" :href="site.docsUrl" target="_blank" rel="noopener" @click="close">
+          接口文档
+        </a>
       </nav>
 
       <div class="header__actions">
+        <a class="btn btn--ghost" :href="site.consoleUrl">登录</a>
         <a class="btn btn--ghost" :href="site.docsUrl" target="_blank" rel="noopener">接口文档</a>
-        <a class="btn btn--primary" :href="site.registerUrl" target="_blank" rel="noopener">商户入驻</a>
+        <a class="btn btn--primary" :href="site.registerUrl">商户入驻</a>
         <button class="nav-toggle" type="button" aria-label="菜单" @click="open = !open">☰</button>
       </div>
     </div>
